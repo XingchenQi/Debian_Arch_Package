69ce4aa3b8c7c85ed7a0b96e7fc30f6544cd336f
Xingchen Qi <XingchenQi@users.noreply.github.com>
2023-07-23 00:04:15 -0500

    delete stuff

--- debian_package_stats.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "enter architectire you wish to view stats on: "
-read -p ":" arch
-echo "wait one moment will we pull the stats for $arch"
-va=$(python3 debian_package_stats.py $arch)
-echo "$va"
-
